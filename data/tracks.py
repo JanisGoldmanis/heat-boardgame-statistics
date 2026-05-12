3987fe5da399db45c32d45dcd61078f3b6ba3119
Janis <janis.goldmanis@gmail.com>
2026-05-12 22:50:01 +0300

    track info

---
+++ data/tracks.py
@@ -0,0 +1,25 @@
+# FINISH - Spaces after Finish Line till first corner.
+# Finish Line is always located on first straight.
+
+TRACKS = {
+    "USA": {
+        "STRAIGHTS": [24, 21, 16, 8],
+        "CORNERS": [7, 3, 3, 2],
+        "FINISH": 11
+    },
+    "FRANCE": {},
+    "GERMANY": {},
+    "GREAT BRITAIN": {},
+    "ITALY": {},
+    "JAPAN": {
+        "STRAIGHTS": [17, 2, 6, 27, 8],
+        "CORNERS": [4, 4, 2, 5, 3],
+        "FINISH": 9
+    },
+    "MEXICO": {},
+    "NETHERLANDS": {},
+    "SPAIN": {},
+    "SOUTH AFRICA": {},
+}
+
+
